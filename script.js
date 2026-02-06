40f481d5ecc3490896bb26f80f696d61cecb8613
NoodleZN <zinnguyen1011@gmail.com>
2026-02-07 01:57:40 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 
+let noClicks = 0;
+
+const noTexts = [
+  "No 😢",
+  "Are you sure?",
+  "Kasar"
+  "Really sure??",
+  "Please reconsider 🥺",
+  "I’ll cry 😭",
+  "Last chance 💔"
+  "No"
+  "Please Mommy"
+  "Yk you want to say yes MAMACITA"
+  "PLEASEEEEEE SAY YES"
+  "Stop being stubborn"
+  "I see..."
+  "YOU HATE MEEEE"
+  "WOW :("
+  "No >:("
+];
+
 yesBtn.onclick = () => {
-  // Confetti burst 🎉
   confetti({
     particleCount: 200,
     spread: 80,
     origin: { y: 0.6 }
   });
 
-  // Cute response
-  yesBtn.textContent = "Yay!! 💖🥰";
+  yesBtn.textContent = "YAYYY 💖🥰";
   noBtn.style.display = "none";
 };
 
 noBtn.onclick = () => {
-  noBtn.style.position = "absolute";
-  noBtn.style.left = Math.random() * 80 + "vw";
-  noBtn.style.top = Math.random() * 80 + "vh";
+  noClicks++;
+
+  // Change No text
+  if (noClicks < noTexts.length) {
+    noBtn.textContent = noTexts[noClicks];
+  }
+
+  // Make Yes button bigger
+  const scale = 1 + noClicks * 0.2;
+  yesBtn.style.transform = `scale(${scale})`;
 };
